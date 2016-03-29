c5c566d443b99455774c4b281a1e55ae94a3be89
rayjuneWu <wuleijun8@gmail.com>
2016-03-29 14:33:42 +0800

    Initial Commit

---
+++ Zeus/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Zeus
+//
+//  Created by 吴蕾君 on 16/3/29.
+//  Copyright © 2016年 rayjuneWu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
