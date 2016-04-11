cbc3809c84fed1ee12f7573c32e2b07f0a334e64
rayjuneWu <wuleijun8@gmail.com>
2016-04-11 18:38:00 +0800

    1.关于我们。 2.意见反馈。

---
+++ Zeus/ViewControllers/我的/AboutViewController.swift
@@ -0,0 +1,38 @@
+//
+//  AboutViewController.swift
+//  Zeus
+//
+//  Created by 吴蕾君 on 16/4/11.
+//  Copyright © 2016年 rayjuneWu. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
