8a4f1ba1d94b07fa36a95d28f25daa1a09d5cbfe
rayjuneWu <wuleijun8@gmail.com>
2016-04-12 18:37:51 +0800

    注册验证码

--- Zeus/ViewControllers/VerifyMobileVC.swift
+++ Zeus/ViewControllers/VerifyMobileVC.swift
@@ -13,7 +13,15 @@ class VerifyMobileVC: BaseViewController {
     @IBOutlet weak var nextButton: UIButton!
     @IBOutlet weak var mobileLabel: UILabel!
     @IBOutlet weak var codeTextField: BorderTextField!
+    @IBOutlet weak var resendButton: UIButton!
+    
     var mobile:String?
+    
+    private var second = ZeusConfig.resendVerifyCodeSeconds()
+    private lazy var timer:NSTimer = {
+        let timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(timerFired), userInfo: nil, repeats: true)
+        return timer
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +30,41 @@ class VerifyMobileVC: BaseViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        resendButton.enabled = false
+    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         codeTextField.becomeFirstResponder()
+        timer.fire()
+    }
+    
+    func timerFired() {
+        if second > 1 {
+            second = second - 1
+            resendButton.enabled = false
+            UIView.performWithoutAnimation({ 
+                self.resendButton.setTitle("重新获取(\(self.second))", forState: .Normal)
+                self.resendButton.layoutIfNeeded()
+            })
+            
+        }else{
+            UIView.performWithoutAnimation({ 
+                self.resendButton.setTitle("重新获取", forState: .Normal)
+                self.resendButton.layoutIfNeeded()
+            })
+            resendButton.enabled = true
+        }
     }
     
     // MARK: Actions
     @IBAction func next_Touch(sender: AnyObject) {
         self.performSegueWithIdentifier("showRegisterUserInfoVC", sender: nil)
+    }
+    
+    @IBAction func resendVerifyCode_Touch(sender: AnyObject) {
     }
     
     @IBAction func textFieldDidChanged(textField: UITextField) {
---
+++ Zeus/ZeusConfig.swift
@@ -0,0 +1,22 @@
+//
+//  ZeusConfig.swift
+//  Zeus
+//
+//  Created by 吴蕾君 on 16/4/12.
+//  Copyright © 2016年 rayjuneWu. All rights reserved.
+//
+
+import UIKit
+
+class ZeusConfig {
+    
+    /**
+     重新获取验证的时间
+     
+     - returns: Int
+     */
+    class func resendVerifyCodeSeconds() -> Int {
+        return 50
+    }
+    
+}
