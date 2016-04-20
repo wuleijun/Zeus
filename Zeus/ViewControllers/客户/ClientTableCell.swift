9d584dd34988cda10d17987fc6184045fb86a73c
rayjuneWu <wuleijun8@gmail.com>
2016-04-20 15:19:54 +0800

    我的客户主页面

--- Zeus/ViewControllers/客户/ClientTableCell.swift
+++ Zeus/ViewControllers/客户/ClientTableCell.swift
@@ -1,29 +1,30 @@
 //
-//  BaseTableViewCell.swift
+//  ClientTableCell.swift
 //  Zeus
 //
-//  Created by 吴蕾君 on 16/3/29.
+//  Created by 吴蕾君 on 16/4/20.
 //  Copyright © 2016年 rayjuneWu. All rights reserved.
 //
 
 import UIKit
 
-
-class BaseTableViewCell: UITableViewCell {
-
-    class func heightOfCell()->CGFloat {
-        return 50
+class ClientTableCell: BaseTableViewCell {
+    
+    @IBOutlet weak var headImageView: UIImageView!
+    
+    override class func heightOfCell()->CGFloat {
+        return 100
     }
-  
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
 }
--- Zeus/ViewControllers/客户/ClientViewController.swift
+++ Zeus/ViewControllers/客户/ClientViewController.swift
@@ -10,4 +10,65 @@ import UIKit
 
 class ClientViewController: BaseViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!{
+        didSet{
+            searchBar.searchFieldBackgroundPositionAdjustment = UIOffset(horizontal: 10, vertical: 0)
+        }
+    }
+    @IBOutlet weak var tableView: UITableView!{
+        didSet{
+            tableView.backgroundColor = UIColor.clearColor()
+            tableView.tableFooterView = UIView()
+            tableView.keyboardDismissMode = .OnDrag
+            tableView.rowHeight = ClientTableCell.heightOfCell()
+            tableView.sectionIndexBackgroundColor = UIColor.clearColor()
+        }
+    }
+    
+    let clientTableCellId = "ClientTableCell"
+    let sectionTitles = ["A","B","C","D","E","F","G"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.registerNib(UINib(nibName: clientTableCellId, bundle: nil), forCellReuseIdentifier: clientTableCellId)
+    }
+}
+
+extension ClientViewController : UITableViewDataSource {
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sectionTitles.count
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let clientCell = tableView.dequeueReusableCellWithIdentifier(clientTableCellId) as! ClientTableCell
+        return clientCell
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionTitles[section]
+    }
+    
+    func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]? {
+        return sectionTitles
+    }
+    
+    func tableView(tableView: UITableView, sectionForSectionIndexTitle title: String, atIndex index: Int) -> Int {
+        return index
+    }
+}
+
+extension ClientViewController : UITableViewDelegate {
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        defer{
+            tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        }
+    }
+    
 }
